5777ece72869f0d83de6b005c4bd92ac8b0a2242
Saikrish <vijayasatyad@gmail.com>
2026-04-08 11:26:35 +0530

    code1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical-chatbot',
+    version='0.1.0',
+    author='Sai krish',
+    author_email= 'vijayasatyad@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
